24660e8b9dc2f3879644a82b192bec9052e93412
AyoubBEsiee <ayoub.benheddi@edu.esiee.fr>
2023-06-21 22:49:31 +0200

    timetable

--- MealPlannerScreen.js
+++ MealPlannerScreen.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import MealPlanContext from './MealPlanContext';
 
 export default function MealPlannerScreen() {
   const { mealPlan } = useContext(MealPlanContext);
+  const [selectedDay, setSelectedDay] = useState(null);
 
   const renderMealItem = ({ item }) => (
     <View style={styles.mealItem}>
@@ -12,8 +14,16 @@ export default function MealPlannerScreen() {
     </View>
   );
 
+  const handleDayChange = (day) => {
+    setSelectedDay(day);
+  };
+
   const renderDayItem = ({ item }) => {
     const meals = item.meals;
+
+    if (item.day !== selectedDay) {
+      return null;
+    }
 
     return (
       <View style={styles.dayItem}>
@@ -69,6 +79,16 @@ export default function MealPlannerScreen() {
 
   return (
     <View style={styles.container}>
+      <Picker
+        selectedValue={selectedDay}
+        onValueChange={handleDayChange}
+        style={styles.picker}
+      >
+        <Picker.Item label="Select a day" value={null} />
+        {mealPlanData.map((item) => (
+          <Picker.Item key={item.day} label={item.day} value={item.day} />
+        ))}
+      </Picker>
       <FlatList
         data={mealPlanData}
         renderItem={renderDayItem}
@@ -82,6 +102,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+  },
+  picker: {
+    marginBottom: 16,
   },
   dayItem: {
     marginBottom: 16,
