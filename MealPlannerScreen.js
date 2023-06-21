02170a9806b7ee1047193b61956f1a5b20a89e1d
AyoubBEsiee <ayoub.benheddi@edu.esiee.fr>
2023-06-21 22:27:17 +0200

    MealPlan v3

--- MealPlannerScreen.js
+++ MealPlannerScreen.js
@@ -1,29 +1,79 @@
 import React, { useContext } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import MealPlanContext from './MealPlanContext';
 
 export default function MealPlannerScreen() {
   const { mealPlan } = useContext(MealPlanContext);
 
+  const renderMealItem = ({ item }) => (
+    <View style={styles.mealItem}>
+      <Text style={styles.foodLabel}>{item.food}</Text>
+      <Text style={styles.calories}>{item.calories} Calories</Text>
+    </View>
+  );
+
+  const renderDayItem = ({ item }) => {
+    const meals = item.meals;
+
+    return (
+      <View style={styles.dayItem}>
+        <Text style={styles.day}>{item.day}</Text>
+        {meals.Breakfast && (
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Breakfast</Text>
+            <FlatList
+              data={meals.Breakfast}
+              renderItem={renderMealItem}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+        )}
+        {meals.Lunch && (
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+            <FlatList
+              data={meals.Lunch}
+              renderItem={renderMealItem}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+        )}
+        {meals.Dinner && (
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+            <FlatList
+              data={meals.Dinner}
+              renderItem={renderMealItem}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+        )}
+        {meals.Snack && (
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Snack</Text>
+            <FlatList
+              data={meals.Snack}
+              renderItem={renderMealItem}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+        )}
+      </View>
+    );
+  };
+
+  const mealPlanData = Object.entries(mealPlan).map(([day, meals]) => ({
+    day,
+    meals,
+  }));
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Meal Planner</Text>
-      {Object.entries(mealPlan).map(([day, meals]) => (
-        <View key={day} style={styles.dayContainer}>
-          <Text style={styles.day}>{day}</Text>
-          {Object.entries(meals).map(([meal, items]) => (
-            <View key={meal} style={styles.mealContainer}>
-              <Text style={styles.meal}>{meal}</Text>
-              {items.map((item, index) => (
-                <View key={index} style={styles.itemContainer}>
-                  <Text>{item.food}</Text>
-                  <Text>Calories: {item.calories}</Text>
-                </View>
-              ))}
-            </View>
-          ))}
-        </View>
-      ))}
+      <FlatList
+        data={mealPlanData}
+        renderItem={renderDayItem}
+        keyExtractor={(item) => item.day}
+      />
     </View>
   );
 }
@@ -31,30 +81,33 @@ export default function MealPlannerScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    padding: 16,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  dayContainer: {
-    marginBottom: 20,
+  dayItem: {
+    marginBottom: 16,
   },
   day: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 8,
   },
   mealContainer: {
-    marginLeft: 10,
+    marginBottom: 8,
   },
-  meal: {
-    fontSize: 14,
+  mealTitle: {
+    fontSize: 16,
     fontWeight: 'bold',
-    marginBottom: 5,
+    marginBottom: 4,
   },
-  itemContainer: {
-    marginLeft: 20,
+  mealItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 4,
+  },
+  foodLabel: {
+    flex: 1,
+  },
+  calories: {
+    marginLeft: 8,
   },
 });
